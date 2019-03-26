ff0aa1247073b06bc5206916c74f2fb42889f1bf
troy tompkins <ttompkasu@gmail.com>
2019-03-26 15:05:36 -0700

    added linear model files and ran first terrible models

---
+++ .ipynb_checkpoints/linear_plotting-checkpoint.py
@@ -0,0 +1,223 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy.stats import probplot
+from statsmodels.graphics.gofplots import ProbPlot
+import statsmodels as sm
+import numpy as np
+
+
+def linearmodel_plots(LMresults, sample_n=None, height=3, width=6):
+    ''' 
+    This funtion calls all the linear model plotting functions at once. Only works with the 
+    results from the LinearModel class.
+    ---
+    Input
+        LMresults:  a fitted instance of LinearModel
+    '''
+    
+    # hopefully plots
+    pred_hist_plot(LMresults.predicted, sample_n)
+    lm_plot(LMresults.predicted, LMresults.y_train, sample_n)
+    resid_plot(LMresults.predicted, LMresults.resid, sample_n)
+    resid_plot_norm(LMresults.predicted, LMresults.resid_norm, sample_n)
+    normal_resid_distr(LMresults.resid_norm, sample_n)
+    hetero_plot(LMresults.predicted, LMresults.resid_norm, sample_n)
+    #leverage_plot(LMresults.resid_normal, _dummy_)
+    # end function
+    
+
+def lm_plot(predicted, y_train, sample_n=None, height = 3, width = 6):
+    '''
+    plot predicted vs actual
+    '''
+    plt.style.use('seaborn')
+    
+    # if sampling value turned on then limit data in the output
+    if sample_n != None:
+        if len(predicted) > sample_n: 
+            i = np.random.choice(len(predicted), sample_n, replace=False)
+            predicted = predicted[i]
+            y_train = y_train[i]
+            
+    # 
+    plot_lm = plt.figure(1)
+    plot_lm.set_figheight(height)
+    plot_lm.set_figwidth(width)
+    plt.scatter(predicted, y_train, alpha = 0.5)
+    x = np.linspace(min(predicted), max(predicted), 100)
+    #plt.plot(x, b0 + b1*x, color = 'r')   # - this only works with 1D X values.
+    #plt.plot(x,x, color='red')
+    
+    # labels
+    plot_lm.axes[0].set_title('Predicted vs Actual')
+    plot_lm.axes[0].set_xlabel('Predicted Values')
+    plot_lm.axes[0].set_ylabel('Actual Values')
+    plt.show()
+    # end function
+    
+    
+def resid_plot(predicted, residuals, sample_n=None, height=2, width=4):    
+    '''
+    This plot shows if residuals have non-linear patterns. There could be a non-linear 
+    relationship between predictor variables and an outcome variable and the pattern 
+    could show up in this plot if the model doesn’t capture the non-linear relationship. 
+    If you find equally spread residuals around a horizontal line without distinct patterns, 
+    that is a good indication you don’t have non-linear relationships.
+
+    A good model's data are simulated in a way that meets the regression assumptions very well, 
+    while a bad model's data are not.
+    '''
+    plt.style.use('seaborn')
+    
+    # if sampling value turned on then limit data in the output
+    if sample_n != None:
+        if len(predicted) > sample_n: 
+            i = np.random.choice(len(predicted), sample_n, replace=False)
+            predicted = predicted[i]
+            residuals = residuals[i]
+    
+    ## residual plot - x = predicted vs y = observed - actual
+    plot_lm_r = plt.figure(1)
+    plot_lm_r.set_figheight(height)
+    plot_lm_r.set_figwidth(width)
+    plot_lm_r.axes[0] = sns.residplot(predicted, residuals, lowess=True, scatter_kws={'alpha':0.5}, line_kws={'color': 'red', 'lw': 2})
+    plot_lm_r.axes[0].set_title('Predicted vs Residuals')
+    plot_lm_r.axes[0].set_xlabel('Predicted Values')
+    plot_lm_r.axes[0].set_ylabel('SalesPrice Model Residuals (obs - pred)')
+    plt.show()
+    # end function
+    
+
+def resid_plot_norm(predicted, norm_resid, sample_n=None, height=2, width=4):    
+    '''
+    This plot shows if normalized residuals have non-linear patterns. There could be a non-linear 
+    relationship between predictor variables and an outcome variable and the pattern 
+    could show up in this plot if the model doesn’t capture the non-linear relationship. 
+    If you find equally spread residuals around a horizontal line without distinct patterns, 
+    that is a good indication you don’t have non-linear relationships.
+
+    A good model's data are simulated in a way that meets the regression assumptions very well, 
+    while bad model's data are not.
+    '''
+    plt.style.use('seaborn')
+    
+    # if sampling value turned on then limit data in the output
+    if sample_n != None:
+        if len(predicted) > sample_n: 
+            i = np.random.choice(len(predicted), sample_n, replace=False)
+            predicted = predicted[i]
+            norm_resid = norm_resid[i]
+    
+    ## residual plot - x = predicted vs y = observed - actual
+    plot_lm_r = plt.figure(1)
+    plot_lm_r.set_figheight(height)
+    plot_lm_r.set_figwidth(width)
+    plot_lm_r.axes[0] = sns.residplot(predicted, norm_resid, lowess=True, scatter_kws={'alpha':0.5}, line_kws={'color': 'red', 'lw': 2})
+    plot_lm_r.axes[0].set_title('Predicted vs Normalized Residuals')
+    plot_lm_r.axes[0].set_xlabel('Predicted Values')
+    plot_lm_r.axes[0].set_ylabel('SalesPrice Model Residuals (obs - pred)')
+    plt.show()
+    # end function
+    
+
+def normal_resid_distr(norm_resid, sample_n=None, height=2, width=4):
+    '''
+    This plot shows if residuals are normally distributed. 
+    Do residuals follow a straight line well or do they deviate severely? 
+    It’s good if residuals are lined well on the straight dashed line.
+    '''
+    plt.style.use('seaborn')
+    
+    # if sampling value turned on then limit data in the output
+    if sample_n != None:
+        if len(norm_resid) > sample_n: 
+            i = np.random.choice(len(norm_resid), sample_n, replace=False)
+            norm_resid = norm_resid[i]
+    
+    ## residual normal quantile plot
+    norm_q_plot = ProbPlot(norm_resid)
+    plot_lm_q = norm_q_plot.qqplot(line='45', alpha=0.2, color='#4C72B0', lw=1)
+    plot_lm_q.set_figheight(height)
+    plot_lm_q.set_figwidth(width)
+    # labels
+    plot_lm_q.axes[0].set_title('Normal Quantile')
+    plot_lm_q.axes[0].set_xlabel('Theoretical Quantiles')
+    plot_lm_q.axes[0].set_ylabel('Normalized Residuals')
+    plt.show()
+    # end function
+
+
+def hetero_plot(predicted, norm_resid, sample_n=None, height=2, width=4):
+    '''
+    This plot shows if residuals are spread equally along the ranges of predictors. This 
+    is how you can check the assumption of equal variance (homoscedasticity). It’s good 
+    if you see a horizontal line with equally (randomly) spread points.
+    '''
+    plt.style.use('seaborn')
+    
+    # if sampling value turned on then limit data in the output
+    if sample_n != None:
+        if len(predicted) > sample_n: 
+            i = np.random.choice(len(predicted), sample_n, replace=False)
+            predicted = predicted[i]
+            norm_resid = norm_resid[i]
+    
+    ## assess heteroscedasticity using absolute squarerooted normalized residuals
+    # heteroscedasticity
+    norm_sqrt_abs_resid_values = np.sqrt(np.abs(norm_resid))
+    plot_lm_h = plt.figure(3)
+    plot_lm_h.set_figheight(height)
+    plot_lm_h.set_figwidth(width)
+    plt.scatter(predicted, np.sqrt(np.abs(norm_resid)), alpha = 0.2)
+    sns.regplot(predicted, np.sqrt(np.abs(norm_resid)),
+                    scatter = False, ci = False, lowess = True,
+                    line_kws ={'color': 'red', 'lw':2, 'alpha': 0.8})
+    plot_lm_h.axes[0].set_title('Heteroscedasticity')
+    plot_lm_h.axes[0].set_xlabel('Predicted Values')
+    plot_lm_h.axes[0].set_ylabel('sqrt abs normalized residuals')
+    plt.show()
+    # end function
+    
+
+def leverage_plot(norm_resid, _dummy_, height=2, width=4):
+    '''
+    this plot not operational:
+     - need values for this from statsmodels:   model.get_influence().hat_matrix_diag 
+    '''
+    plt.style.use('seaborn')
+    
+    # if sampling value turned on then limit data in the output
+    if sample_n != None:
+        if len(norm_resid) > sample_n: 
+            i = np.random.choice(len(norm_resid), sample_n, replace=False)
+            #predicted = predicted[i]
+            norm_resid = norm_resid[i]
+    
+    ## leverage - outlier influence
+    plot_lm_o = plt.figure(4)
+    plot_lm_o.set_figheight(height)
+    plot_lm_o.set_figwidth(width)
+    #plt.scatter(model.get_influence().hat_matrix_diag, norm_resid, alpha = 0.2)
+    #sns.regplot(model.get_influence().hat_matrix_diag, norm_resid, scatter=False, ci=False, lowess=True,line_kws ={'color': 'red', 'lw':2, 'alpha': 0.8})
+    #plot_lm_o.axes[0].set_xlim(0,0.20)
+    #plot_lm_o.axes[0].set_ylim(-3, 5)
+    plot_lm_o.axes[0].set_title('Outlier Leverage')
+    plot_lm_o.axes[0].set_xlabel('Leverage')
+    plot_lm_o.axes[0].set_ylabel('Normalized Residuals')
+    plt.show()
+    # end function
+    
+    
+def pred_hist_plot(predicted, sample_n=None, height=2, width=4):
+    
+    # if sampling value turned on then limit data in the output
+    if sample_n != None:
+        if len(predicted) > sample_n: 
+            i = np.random.choice(len(predicted), sample_n, replace=False)
+            predicted = predicted[i]
+    
+    plt.hist(predicted, bins=30, rwidth=0.9)
+    plt.xlabel("predicted y")
+    plt.ylabel("counts")
+    plt.show()
+    # end function
---
+++ linear_model.py
@@ -0,0 +1,185 @@
+from sklearn.linear_model import LinearRegression, Lasso, Ridge
+from sklearn.model_selection import train_test_split, cross_validate, cross_val_score
+from sklearn.metrics import mean_squared_error, r2_score
+import numpy as np
+import pandas as pd
+
+class LinearModel():
+    '''
+    Linear_model class
+    Inputs data, splits into training/test, fits model, saves results
+        input:
+            model_type: reqd. 'linear', 'lasso', 'ridge'
+            X:          reqd. numpy array or pandas df.
+            y:          reqd. numpy array. target variable
+            split:      optional. boolean. default = True. Splits the X and y variables 
+                        to 'train' and 'test' sets 
+            test_size:  optional. float. default = 0.25. Train/test split size (test split proportion)
+            alpha:      optional. list. default = [1]. List of alphas to use in the ridge and 
+                        lasso regressions
+            cv:         optional. int. If >1, run cross validation with cv folds and output the 
+                        mean score
+        output:
+            train_pred: 1d array of n predictions, for every point in the training set
+            resid:      1d array of n residuals, for every point in the training set
+            norm_resid: 1d array of n residuals, normalized
+            test_pred:  if X_test is passed in, 1d array of predictions for every point in the test set
+            rmse:       float root mean squared error
+            cv_score:   float. if cv is passed in, mean cv score
+            intercept:  float. 
+            coef:       d size array of coefficients
+            r2:         r-squared
+            r2_adj:     adjusted R-squared
+    '''
+
+    def __init__(self, model_type, X, y, split=True, test_size=0.25, alpha = [1], cv = 5):
+        # attributes
+        self.model_type = model_type
+        self.X = X
+        self.y = y
+        self.alpha = alpha
+        self.cv = cv
+        self.test_size = test_size
+        self.model = None
+        self.X_train = X   # set to X in case data not split for training 
+        self.X_test = None
+        self.y_train = y   # set to y in case data not split for training
+        self.y_test = None
+        self.predicted = None
+        self.test_pred = pd.DataFrame
+        self.resid = None
+        self.resid_norm = None
+        self.rmse = None
+        self.rmse_log = None
+        self.coef = None
+        self.intercept = None
+        self.cv_score = None
+        self.a_max = None
+        self.r2 = None
+        self.r2_adj = None
+        self.score = None
+
+        # check that the X and y data are shaped properly.
+        if self.X.ndim == 1:
+            self.X = np.array(X).reshape(-1,1)
+        if self.y.ndim == 1:
+            self.y = np.array(y).reshape(-1,1)
+
+        ## auto-run methods
+        # split data
+        if split == True:
+            self.splitter()
+        
+        # run the specified regression
+        if self.model_type == 'lasso':
+            self.lasso_pred()
+        elif self.model_type == 'ridge':
+            self.ridge_pred()
+        elif self.model_type == "linear":
+            self.linear_pred()
+        else:
+            print("There is something wrong with your model selection. Chose 'linear', 'lasso', or 'ridge'")
+        
+        # save the results
+        self.results()
+        # end function
+    
+
+    def splitter(self):
+        '''
+        split X and y into train/test
+        '''
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split( self.X, self.y, test_size=self.test_size, random_state=1234)
+    
+
+    def linear_pred(self):
+        """
+        linear regression
+        input:
+            model: fits sklearn's LinearRegression()
+            X_train: nxd array
+            y_train: n-size 1D array
+            X_test: nxd array. Optional
+            cv: int optional. If >0, run cross validation with cv folds and output the mean score
+        """
+        # enter cross-validation function.
+        self.model = LinearRegression()
+        self.model.fit(self.X_train,self.y_train)
+        # end function
+
+
+    def ridge_pred(self):
+        """
+        ridge regression.
+        get optimal alpha then get predictions and metrics using that alpha.
+        input:
+            X_train:   nxd array
+            y_train:   n-size 1D array
+            alphas:    list of floats, alphas to test.
+            X_test:    nxd array. Optional
+            cv:        int optional. If >1, run cross validation with cv folds and 
+                       output the mean score
+        """
+        temp_cv_scores = np.ones(len(self.alpha))
+        for idx, a in enumerate(self.alpha):
+            temp_model = Ridge(alpha = a)
+            #temp_model.fit(self.X_train, self.y_train)
+            temp_cv_scores[idx] = (cross_val_score(temp_model, 
+                                                   self.X_train, 
+                                                   self.y_train, 
+                                                   cv = self.cv)).mean()
+        self.a_max = self.alpha[temp_cv_scores.argmax()]
+        self.model = Ridge(alpha = self.a_max)
+        self.model.fit(self.X_train, self.y_train)
+        # end function
+
+
+    def lasso_pred(self):
+        '''
+        lasso regression
+        get optimal alpha then get predictions and metrics using that alpha
+        '''
+        temp_cv_scores = np.ones(len(self.alpha))
+        for idx, a in enumerate(self.alpha):
+            temp_model = Lasso(alpha = a)
+            temp_model.fit(self.X_train, self.y_train)
+            temp_cv_scores[idx] = (cross_val_score(temp_model, 
+                                                   self.X_train, 
+                                                   self.y_train, 
+                                                   cv = self.cv)).mean()
+        self.a_max = self.alpha[temp_cv_scores.argmax()]
+        self.model = Lasso(alpha = self.a_max)
+        self.model.fit(self.X_train, self.y_train)
+        # end function
+
+        
+    def test_predict(self, X_test=pd.DataFrame()):
+        if len(X_test.index) == 0:
+            self.test_pred = self.model.predict(self.X_test)
+        else:
+            self.test_pred = self.model.predict(X_test)
+        return self.test_pred
+        
+
+    def results(self):
+        '''
+        determines the model fit metrics and saves them to the class object
+        '''
+        self.y_train = self.y_train.flatten()
+        self.predicted = self.model.predict(self.X_train).flatten()
+        self.resid = self.predicted - self.y_train
+        self.resid_norm = (self.resid - self.resid.mean()) / self.resid.std()
+        self.rmse = np.sqrt(mean_squared_error(self.y_train, self.predicted))
+        self.rmse_log = np.sqrt(((( np.log(self.predicted+1)-np.log(self.y_train + 1) )**2).sum() ) / len(self.y_train))
+        self.coef = self.model.coef_
+        self.intercept = self.model.intercept_
+        self.r2 = r2_score(self.y_train, self.predicted)
+        self.r2_adj = 1 - (1-self.r2)*(len(self.y_train)-1)/(len(self.y_train)-self.X_train.shape[1]-1)
+        self.score = self.model.score(self.X_test, self.y_test)
+
+        if self.X_test is not None:
+            self.test_pred = self.model.predict(self.X_test)
+
+        if self.cv > 1:
+            self.cv_score = (cross_val_score(self.model, self.X_train, self.y_train, cv = self.cv)).mean()
+        # end function
