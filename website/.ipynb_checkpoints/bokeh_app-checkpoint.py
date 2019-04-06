98822c942ef7ce4b5d4a5436c2b8939c37bb0606
troy tompkins <ttompkasu@gmail.com>
2019-04-05 23:13:27 -0700

    bokeh connecting dots

--- website/.ipynb_checkpoints/bokeh_app-checkpoint.py
+++ website/.ipynb_checkpoints/bokeh_app-checkpoint.py
@@ -1,4 +1,8 @@
 #bokeh_app
+from os.path import dirname, join
+from datetime import date
+from datetime import timedelta as td
+from datetime import datetime as dt
 
 from bokeh.io import output_file, show
 from bokeh.plotting import curdoc
@@ -6,36 +10,65 @@ from bokeh.layouts import widgetbox, row, gridplot, layout
 from bokeh.layouts import column as bokehcolumn
 from bokeh.plotting import figure
 from bokeh.models.widgets import TextInput, Paragraph, Button, MultiSelect, RadioGroup
-from bokeh.models.widgets import DatePicker, Panel, Tabs, Div
-from datetime import date
-from datetime import timedelta as td
-from datetime import datetime as dt
+from bokeh.models.widgets import DatePicker, Panel, Tabs, Div, RadioButtonGroup, Button
 
 from bokeh.palettes import Viridis3
 
-
 output_file("text_input.html")
 
-p = Paragraph(text="""Your text is initialized with the 'text' argument.  The
-remaining Paragraph arguments are 'width' and 'height'. For this example, those values
-are 200 and 100 respectively.""",
-width=200, height=100)
+desc = Div(text=open(join(dirname(__file__), "description.html")).read(), width=800)
 
-#show(widgetbox(p))
 
-subject_id = TextInput(value="default", title="Subject ID:")
-date_onset = TextInput(value="default", title="Date of ALS Onset")
-onset_loc = RadioGroup(labels=["Bulbar", "Spinal", "Both"], active=0)
+## Onset Panel
+dt_date_onset=DatePicker(title="Date of ALS Onset", 
+                                       min_date=date(1990,1,1),
+                                       max_date=date.today())
+subject_id = TextInput(title="Subject ID:", value="default")
+#date_onset = TextInput(title="Date of ALS Onset", value="default")
 onset_loc_detail = MultiSelect(title="Location Detail (can pick multiple)", value=["unk"],
                            options=[("unk", "unknown"),("hands", "Hands"), 
                                     ("arms", "Arms"), ("feet", "Feet"), ("legs", "Legs")])
 
+# onset radio button
+def onset_loc_handler(new):
+    print ('Radio button option ' + str(new) + ' selected.')
+    
+pp_onset = Paragraph(text="""Onset Neuron Group""", width=250, height=15)
+onset_loc = RadioButtonGroup(labels=["Bulbar", "Spinal", "Both"], active=0)
+onset_loc.on_click(onset_loc_handler)
+
+# riluzole radio button
+def riluzole_handler(new):
+    print ('Radio button option ' + str(new) + ' selected.')
+    
+pp_riluzole = Paragraph(text="""Subject_used_Riluzole""", width=250, height=15)
+riluzole = RadioButtonGroup(labels=["Yes", "No"], active=0)
+riluzole.on_click(onset_loc_handler)
+
+# caucasian radio button
+def caucasian_handler(new):
+    print ('Radio button option ' + str(new) + ' selected.')
+    
+pp_caucasian = Paragraph(text="""Race Caucasian""", width=250, height=15)
+caucasian = RadioButtonGroup(labels=["Yes", "No"], active=0) 
+caucasian.on_click(caucasian_handler)
+
+# sex radio button
+def sex_handler(new):
+    print ('Radio button option ' + str(new) + ' selected.')
+    
+pp_sex = Paragraph(text="""Sex""", width=250, height=15)
+sex = RadioButtonGroup(labels=["Male", "Female"], active=0)
+sex.on_click(sex_handler)
+
+
 dumbdiv = Div()
+
 
 ## ALSFRS panels
 dt_alsfrs_1=DatePicker(title='Date of Test: ', 
                        min_date=date(1990,1,1),
-                       max_date=date.today(), width=150)
+                       max_date=date.today(), width=100)
 dt_alsfrs_2=DatePicker(title='Date of 2nd ALSFRS: ', 
                                        min_date=date(1990,1,1),
                                        max_date=date.today())
@@ -51,106 +84,111 @@ dt_alsfrs_5=DatePicker(title='Date of 5th ALSFRS: ',
 dt_alsfrs_6=DatePicker(title='Date of 6th ALSFRS: ', 
                                        min_date=date(1990,1,1),
                                        max_date=date.today())
+wdbox=40
+Q1_1 = TextInput(value="", title="Q1:", width=wdbox)
+Q1_2 = TextInput(value="", title="Q1:")
+Q1_3 = TextInput(value="", title="Q1:")
+Q1_4 = TextInput(value="", title="Q1:")
+Q1_5 = TextInput(value="", title="Q1:")
+Q1_6 = TextInput(value="", title="Q1:")
 
-Q1_1 = TextInput(value="", title="Q1:")
-Q1_2 = TextInput(value="default", title="Label:", width=20)
-Q1_3 = TextInput(value="default", title="Label:", width=20)
-Q1_4 = TextInput(value="default", title="Label:", width=20)
-Q1_5 = TextInput(value="default", title="Label:", width=20)
-Q1_6 = TextInput(value="default", title="Label:", width=20)
+Q2_1 = TextInput(value="", title="Q2:", width=wdbox)
+Q2_2 = TextInput(value="", title="Q2:")
+Q2_3 = TextInput(value="", title="Q2:")
+Q2_4 = TextInput(value="", title="Q2:")
+Q2_5 = TextInput(value="", title="Q2:")
+Q2_6 = TextInput(value="", title="Q2:")
 
-Q2_1 = TextInput(value="", title="Q2:")
-Q2_2 = TextInput(value="default", title="Label:")
-Q2_3 = TextInput(value="default", title="Label:")
-Q2_4 = TextInput(value="default", title="Label:")
-Q2_5 = TextInput(value="default", title="Label:")
-Q2_6 = TextInput(value="default", title="Label:")
+Q3_1 = TextInput(value="", title="Q3:", width=wdbox)
+Q3_2 = TextInput(value="", title="Q3:")
+Q3_3 = TextInput(value="", title="Q3:")
+Q3_4 = TextInput(value="", title="Q3:")
+Q3_5 = TextInput(value="", title="Q3:")
+Q3_6 = TextInput(value="", title="Q3:")
 
-Q3_1 = TextInput(value="", title="Q3:")
-Q3_2 = TextInput(value="default", title="Label:")
-Q3_3 = TextInput(value="default", title="Label:")
-Q3_4 = TextInput(value="default", title="Label:")
-Q3_5 = TextInput(value="default", title="Label:")
-Q3_6 = TextInput(value="default", title="Label:")
+Q4_1 = TextInput(value="", title="Q4:", width=wdbox)
+Q4_2 = TextInput(value="", title="Q4:")
+Q4_3 = TextInput(value="", title="Q4:")
+Q4_4 = TextInput(value="", title="Q4:")
+Q4_5 = TextInput(value="", title="Q4:")
+Q4_6 = TextInput(value="", title="Q4:")
 
-Q4_1 = TextInput(value="", title="Q4:")
-Q4_2 = TextInput(value="default", title="Label:")
-Q4_3 = TextInput(value="default", title="Label:")
-Q4_4 = TextInput(value="default", title="Label:")
-Q4_5 = TextInput(value="default", title="Label:")
-Q4_6 = TextInput(value="default", title="Label:")
+Q5_1 = TextInput(value="", title="Q5:", width=wdbox)
+Q5_2 = TextInput(value="", title="Q5:")
+Q5_3 = TextInput(value="", title="Q5:")
+Q5_4 = TextInput(value="", title="Q5:")
+Q5_5 = TextInput(value="", title="Q5:")
+Q5_6 = TextInput(value="", title="Q5:")
 
-Q5_1 = TextInput(value="", title="Q5:")
-Q5_2 = TextInput(value="default", title="Label:")
-Q5_3 = TextInput(value="default", title="Label:")
-Q5_4 = TextInput(value="default", title="Label:")
-Q5_5 = TextInput(value="default", title="Label:")
-Q5_6 = TextInput(value="default", title="Label:")
+Q6_1 = TextInput(value="", title="Q6:", width=wdbox)
+Q6_2 = TextInput(value="", title="Q6:")
+Q6_3 = TextInput(value="", title="Q6:")
+Q6_4 = TextInput(value="", title="Q6:")
+Q6_5 = TextInput(value="", title="Q6:")
+Q6_6 = TextInput(value="", title="Q6:")
 
-Q6_1 = TextInput(value="", title="Q6:")
-Q6_2 = TextInput(value="default", title="Label:")
-Q6_3 = TextInput(value="default", title="Label:")
-Q6_4 = TextInput(value="default", title="Label:")
-Q6_5 = TextInput(value="default", title="Label:")
-Q6_6 = TextInput(value="default", title="Label:")
+Q7_1 = TextInput(value="", title="Q7:", width=wdbox)
+Q7_2 = TextInput(value="", title="Q7:")
+Q7_3 = TextInput(value="", title="Q7:")
+Q7_4 = TextInput(value="", title="Q7:")
+Q7_5 = TextInput(value="", title="Q7:")
+Q7_6 = TextInput(value="", title="Q7:")
 
-Q7_1 = TextInput(value="", title="Label:")
-Q7_2 = TextInput(value="default", title="Label:")
-Q7_3 = TextInput(value="default", title="Label:")
-Q7_4 = TextInput(value="default", title="Label:")
-Q7_5 = TextInput(value="default", title="Label:")
-Q7_6 = TextInput(value="default", title="Label:")
+Q8_1 = TextInput(value="", title="Q8:", width=wdbox)
+Q8_2 = TextInput(value="", title="Q8:")
+Q8_3 = TextInput(value="", title="Q8:")
+Q8_4 = TextInput(value="", title="Q8:")
+Q8_5 = TextInput(value="", title="Q8:")
+Q8_6 = TextInput(value="", title="Q8:")
 
-Q8_1 = TextInput(value="", title="Label:")
-Q8_2 = TextInput(value="default", title="Label:")
-Q8_3 = TextInput(value="default", title="Label:")
-Q8_4 = TextInput(value="default", title="Label:")
-Q8_5 = TextInput(value="default", title="Label:")
-Q8_6 = TextInput(value="default", title="Label:")
+Q9_1 = TextInput(value="", title="Q9:", width=wdbox)
+Q9_2 = TextInput(value="", title="Q9:")
+Q9_3 = TextInput(value="", title="Q9:")
+Q9_4 = TextInput(value="", title="Q9:")
+Q9_5 = TextInput(value="", title="Q9:")
+Q9_6 = TextInput(value="", title="Q9:")
 
-Q9_1 = TextInput(value="", title="Label:")
-Q9_2 = TextInput(value="default", title="Label:")
-Q9_3 = TextInput(value="default", title="Label:")
-Q9_4 = TextInput(value="default", title="Label:")
-Q9_5 = TextInput(value="default", title="Label:")
-Q9_6 = TextInput(value="default", title="Label:")
+Q10_1 = TextInput(value="(depreciated)", title="Q10:", width=wdbox)
+Q10_2 = TextInput(value="(depreciated)", title="Q10:")
+Q10_3 = TextInput(value="(depreciated)", title="Q10:")
+Q10_4 = TextInput(value="(depreciated)", title="Q10:")
+Q10_5 = TextInput(value="(depreciated)", title="Q10:")
+Q10_6 = TextInput(value="(depreciated)", title="Q10:")
 
-Q10_1 = TextInput(value="", title="Label:")
-Q10_2 = TextInput(value="default", title="Label:")
-Q10_3 = TextInput(value="default", title="Label:")
-Q10_4 = TextInput(value="default", title="Label:")
-Q10_5 = TextInput(value="default", title="Label:")
-Q10_6 = TextInput(value="default", title="Label:")
+R1_1 = TextInput(value="", title="R1:", width=wdbox)
+R1_2 = TextInput(value="", title="Label:")
+R1_3 = TextInput(value="", title="Label:")
+R1_4 = TextInput(value="", title="Label:")
+R1_5 = TextInput(value="", title="Label:")
+R1_6 = TextInput(value="", title="Label:")
 
-R1_1 = TextInput(value="", title="Label:")
-R1_2 = TextInput(value="default", title="Label:")
-R1_3 = TextInput(value="default", title="Label:")
-R1_4 = TextInput(value="default", title="Label:")
-R1_5 = TextInput(value="default", title="Label:")
-R1_6 = TextInput(value="default", title="Label:")
+R2_1 = TextInput(value="", title="R2", width=wdbox)
+R2_2 = TextInput(value="", title="Label:")
+R2_3 = TextInput(value="", title="Label:")
+R2_4 = TextInput(value="", title="Label:")
+R2_5 = TextInput(value="", title="Label:")
+R2_6 = TextInput(value="", title="Label:")
 
-R2_1 = TextInput(value="", title="Label:")
-R2_2 = TextInput(value="default", title="Label:")
-R2_3 = TextInput(value="default", title="Label:")
-R2_4 = TextInput(value="default", title="Label:")
-R2_5 = TextInput(value="default", title="Label:")
-R2_6 = TextInput(value="default", title="Label:")
+R3_1 = TextInput(value="", title="R3", width=wdbox)
+R3_2 = TextInput(value="", title="Label:")
+R3_3 = TextInput(value="", title="Label:")
+R3_4 = TextInput(value="", title="Label:")
+R3_5 = TextInput(value="", title="Label:")
+R3_6 = TextInput(value="", title="Label:")
 
-R3_1 = TextInput(value="", title="Label:")
-R3_2 = TextInput(value="default", title="Label:")
-R3_3 = TextInput(value="default", title="Label:")
-R3_4 = TextInput(value="default", title="Label:")
-R3_5 = TextInput(value="default", title="Label:")
-R3_6 = TextInput(value="default", title="Label:")
 
-#l = layout([ [bollinger],[sliders, plot],[p1, p2, p3],], sizing_mode='scale_width')
+## ALSFRS boxes
+wd = 200
+l1a = row(widgetbox(Q1_1, Q2_1, Q3_1, Q4_1, width=wd), 
+          widgetbox(Q5_1, Q6_1, Q7_1, Q8_1,width=wd), 
+          widgetbox(Q9_1, Q10_1, width=wd), 
+          widgetbox(R1_1, R2_1, R3_1, width=wd))
 
-l1 = layout([dt_alsfrs_1],[Q1_1, Q2_1, Q3_1, Q4_1], [Q5_1, Q6_1], [Q7_1, Q8_1], sizing_mode='fixed',
-           width=)
+l1 = layout([dt_alsfrs_1], l1a, sizing_mode='scale_width')
                  #widgetbox(Q9_1, Q10_1, R1_1)
                  #widgetbox(R2_1, R3_1))),, sizing_mode='fixed')
-l2 = layout([[widgetbox(dt_alsfrs_2, width=100)], Q1_2, Q2_2, Q3_2, Q4_2, Q5_2, Q6_2, Q7_2,
-            Q8_2, Q9_2, Q10_2, R1_2, R2_2, R3_2], sizing_mode='fixed')
+l2 = layout([dt_alsfrs_2, Q1_2, Q2_2, Q3_2, Q4_2, Q5_2, Q6_2, Q7_2,
+            Q8_2, Q9_2, Q10_2, R1_2, R2_2, R3_2], sizing_mode='scale_width')
 
 l3 = layout([dt_alsfrs_3, Q1_3, Q2_3, Q3_3, Q4_3, Q5_3, Q6_3, Q7_3,
             Q8_3, Q9_3, Q10_3, R1_3, R2_3, R3_3], sizing_mode='fixed')
@@ -172,11 +210,14 @@ tab4 = Panel(child= l4, title="ALSFRS-4")
 tab5 = Panel(child= l5, title="ALSFRS-5")
 tab6 = Panel(child= l6, title="ALSFRS-6")
 
-
 tabs = Tabs(tabs=[ tab1, tab2, tab3, tab4, tab5, tab6], width = 800)
 
 text_input = TextInput(value="default", title="this")
 
+button = Button(label="Foo", button_type="success")
+
+
+## button to run prediction
 button = Button(label="Foo", button_type="success")
 
 ## date things
@@ -203,13 +244,19 @@ p3.square(x, y2, size=10, color=Viridis3[2])
 # make a grid
 grid = gridplot([[p1, p2], [None, p3]])
 
-# show the results
+
+### Display
 dt_alsfrs_1.on_change('value',callback)
 #curdoc().add_root(bokehcol(dt_pckr_strt))
 
-show(layout(
+show(layout([desc],
     row(widgetbox(
-        p,subject_id,date_onset, onset_loc, onset_loc_detail, width=200),
+        subject_id, dt_date_onset, pp_onset, 
+        onset_loc, onset_loc_detail, 
+        pp_riluzole, riluzole, 
+        pp_caucasian, caucasian,
+        pp_sex, sex,
+        width=250),
         tabs), 
      grid ))
 
